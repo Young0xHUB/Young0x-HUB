f8eec11d7c699015f08b62bf155fd400471375ca
Young0x <young0xbusiness@gmail.com>
2026-08-25 21:45:29 -0300

    HW1D.lua

--- modules/HW1D.lua
+++ modules/HW1D.lua
@@ -2,4 +2,11 @@ local hwid = gethwid and gethwid()
 
 if hwid and setclipboard then
     setclipboard(hwid)
+    
+
+    game:GetService("StarterGui"):SetCore("SendNotification", {
+        Title = "Young0x Hub",
+        Text = "HWID Copiado.",
+        Duration = 5
+    })
 end
